0e11d67fb9312765ef6f84b5143495f62fe80c5d
Shudfly <35050346+Shudfly@users.noreply.github.com>
2024-04-12 15:33:42 -0700

    more testing

--- bios/main.js
+++ bios/main.js
@@ -1,4 +1,30 @@
-import { initializePage } from "../utils";
-window.onload = () => {
-    initializePage();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
+import { bios, getBioInfo, initializePage } from "../utils.js";
+let peopleList;
+window.onload = () => {
+    peopleList = document.getElementById("people_list");
+    initializePage();
+    getBioInfo("actors").then(() => __awaiter(void 0, void 0, void 0, function* () {
+        parsePeopleEntries();
+    }));
+};
+function parsePeopleEntries() {
+    const personTemplate = peopleList.children[0];
+    bios["actors"].forEach((element) => {
+        var _a, _b;
+        let curEntry;
+        curEntry = personTemplate.cloneNode(true);
+        curEntry.getElementsByClassName("headshot").item(0).src = `./headshots/${element.name.toLowerCase().replace(" ", "")}.jpg`;
+        ((_a = curEntry.getElementsByClassName("person_name").item(0)) !== null && _a !== void 0 ? _a : document.createElement("div")).textContent = element.name;
+        (_b = peopleList.lastElementChild) === null || _b === void 0 ? void 0 : _b.after(curEntry);
+    });
+    personTemplate.remove();
+}
--- bios/main.ts
+++ bios/main.ts
@@ -1,6 +1,34 @@
-import { initializePage } from "../utils"
+import { bios, getBioInfo, initializePage } from "../utils.js";
 
+let peopleList: HTMLDivElement;
 
 window.onload = () => {
+  peopleList = document.getElementById("people_list") as HTMLDivElement;
+
   initializePage();
+
+  getBioInfo("actors").then(async () => {
+    parsePeopleEntries();
+  });
+};
+
+function parsePeopleEntries() {
+  const personTemplate = peopleList.children[0] as HTMLDivElement;
+
+  bios["actors"].forEach((element) => {
+    let curEntry: HTMLElement;
+
+    curEntry = personTemplate.cloneNode(true) as HTMLDivElement;
+    (
+      curEntry.getElementsByClassName("headshot").item(0) as HTMLImageElement
+    ).src = `./headshots/${element.name.toLowerCase().replace(" ", "")}.jpg`;
+    (
+      curEntry.getElementsByClassName("person_name").item(0) ??
+      document.createElement("div")
+    ).textContent = element.name;
+
+    peopleList.lastElementChild?.after(curEntry);
+  });
+
+  personTemplate.remove();
 }
